750490113c918aab64418d7bb696bc2cc96035e3
tanjiuyusila <521196748@qq,com>
2019-06-07 21:00:48 +0800

    注册功能加盐加密

--- shop_server/controller/user.js
+++ shop_server/controller/user.js
@@ -8,8 +8,9 @@ router.post('/registUser', async (ctx) => { //和service.config.js里面一致
     // ctx.body = '请求成功';
     //获取model
     const User = mongoose.model('User');
-    let newUser = new User(ctx.request.body);//接收post请求，封装成user对象
-    await newUser.save().then(() =>{ //使用save保存用户信息
+    let newUser = new User(ctx.request.body);//接收post请求，封装成user对象，body中包含用户名密码
+
+    await newUser.save().then(() =>{ //使用save保存用户信息到数据库
         ctx.body = {
             code:200,
             nessage:'注册成功'
--- shop_server/model/User.js
+++ shop_server/model/User.js
@@ -1,6 +1,8 @@
 //集合,和数据库打交道
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');//
+
 //MVC中的Model，对应这数据库中的表（在mongoDB这种数据库称作集合），每一个属性，对应着mongoDB中的key，
 const userSchema = new Schema({  //每一个属性对应着集合中的字段名称
     userId: Schema.Types.ObjectId,
@@ -8,5 +10,22 @@ const userSchema = new Schema({  //每一个属性对应着集合中的字段名
     password: String,
     createDate: { type: Date, default: Date.now() }
 });
+//加严加密
+userSchema.pre('save',function (next){  //next:调用next进行下一步,不写箭头函数，
+
+    bcrypt.genSalt(10,(err,salt) => { //随机生成盐；10：盐的迭代次数；
+        if(err) return next(err);  //生成盐的时候发生错误了，执行下段代码
+        // console.log(this);
+        bcrypt.hash(this.password,salt,(err,hash) => {
+            // console.log(this);
+            // 参数：对谁加盐，加的参数，hash：加盐成功，hash值就是加盐完成之后的值
+            if(err) return next(err);
+            this.password = hash;  //this指代当前模型，即这个模型中的password
+            next();
+        })
+    });
+});
+
+
 //发布模型
 mongoose.model('User',userSchema);//发布时的模型名字叫做User，所以user.js中引用模型，就用这个名字
